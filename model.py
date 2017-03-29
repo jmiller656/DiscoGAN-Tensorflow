4df9c75dad275039c5729dfcafa3f4b73eadbb33
Josh <jxm7374@rit.edu>
2017-03-28 23:27:41 -0400

    Seems to be functional now

--- model.py
+++ model.py
@@ -1,75 +1,156 @@
 import tensorflow as tf
+import data
 import utils
+import os
+import scipy.misc
 
-LR = 2e-4
-B1 = 0.5
-B2 = 0.999
+with tf.Graph().as_default():
 
-#Input placeholders
-x_a = tf.placeholder(tf.float32,shape=[200,64,64,3],name="xa")
-x_b = tf.placeholder(tf.float32,shape=[200,64,64,3],name="xb")
+	LR = 2e-4
+	B1 = 0.5
+	B2 = 0.999
 
-#Generator Networks
-g_ab = utils.generator(x_a,name="gen_AB")
-g_ba = utils.generator(x_b,name="gen_BA")
+	BATCH_SIZE = 10
 
-#Secondary generator networks, reusing params of previous two
-g_aba = utils.generator(g_ab,name="gen_BA",reuse=True)
-g_bab = utils.generator(g_ba,name="gen_AB",reuse=True)
+	#Input placeholders
+	x_a = tf.placeholder(tf.float32,shape=[BATCH_SIZE,64,64,3],name="xa")
+	x_b = tf.placeholder(tf.float32,shape=[BATCH_SIZE,64,64,3],name="xb")
 
-#Discriminator for input a
-disc_a_real = utils.discriminator(x_a,name="disc_a")
-disc_a_fake = utils.discriminator(g_ba,name="disc_a",reuse=True)
+	#Generator Networks
+	g_ab = utils.generator(x_a,BATCH_SIZE,name="gen_AB")
+	g_ba = utils.generator(x_b,BATCH_SIZE,name="gen_BA")
 
-#Discriminator for input b
-disc_b_real = utils.discriminator(x_b,name="disc_b")
-disc_b_fake = utils.discriminator(g_ab,name="disc_b",reuse=True)
+	#Secondary generator networks, reusing params of previous two
+	g_aba = utils.generator(g_ab,BATCH_SIZE,name="gen_BA",reuse=True)
+	g_bab = utils.generator(g_ba,BATCH_SIZE,name="gen_AB",reuse=True)
 
-#Reconstruction loss for generators
-l_const_a = tf.nn.l2_loss(g_aba-x_a)
-l_const_b = tf.nn.l2_loss(g_bab-x_b)
+	#Discriminator for input a
+	disc_a_real = utils.discriminator(x_a,name="disc_a")
+	disc_a_fake = utils.discriminator(g_ba,name="disc_a",reuse=True)
 
-#Generation loss for generators 
-l_gan_a = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_fake,labels=tf.ones_like(disc_a_fake)))
-l_gan_b = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_fake,labels=tf.ones_like(disc_b_fake)))
+	#Discriminator for input b
+	disc_b_real = utils.discriminator(x_b,name="disc_b")
+	disc_b_fake = utils.discriminator(g_ab,name="disc_b",reuse=True)
 
-#Real example loss for discriminators
-l_disc_a_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_real,labels=tf.ones_like(disc_a_real)))
-l_disc_b_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_real,labels=tf.ones_like(disc_b_real)))
+	#Reconstruction loss for generators
+	l_const_a = tf.reduce_mean(utils.huber_loss(g_aba,x_a))
+	l_const_b = tf.reduce_mean(utils.huber_loss(g_bab,x_b))
 
-#Fake example loss for discriminators
-l_disc_a_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_fake,labels=tf.zeros_like(disc_a_fake)))
-l_disc_b_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_fake,labels=tf.zeros_like(disc_b_fake)))
+	#l_const_a = tf.reduce_mean(tf.nn.l2_loss(g_aba-x_a))
+	#l_const_b = tf.reduce_mean(tf.nn.l2_loss(g_bab-x_b))
 
-#Combined loss for individual discriminators
-l_disc_a = l_disc_a_real + l_disc_a_fake
-l_disc_b = l_disc_b_real + l_disc_b_fake
+	#Generation loss for generators 
+	l_gan_a = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_fake,labels=tf.ones_like(disc_a_fake)))
+	l_gan_b = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_fake,labels=tf.ones_like(disc_b_fake)))
 
-#Total discriminator loss
-l_disc = l_disc_a + l_disc_b
+	#Real example loss for discriminators
+	l_disc_a_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_real,labels=tf.ones_like(disc_a_real)))
+	l_disc_b_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_real,labels=tf.ones_like(disc_b_real)))
 
-#Combined loss for individual generators
-l_ga = l_gan_a + l_const_a
-l_gb = l_gan_b + l_const_b
+	#Fake example loss for discriminators
+	l_disc_a_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_a_fake,labels=tf.zeros_like(disc_a_fake)))
+	l_disc_b_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=disc_b_fake,labels=tf.zeros_like(disc_b_fake)))
 
-#Total GAN loss
-l_g = l_ga + l_gb
+	#Combined loss for individual discriminators
+	l_disc_a = l_disc_a_real + l_disc_a_fake
+	l_disc_b = l_disc_b_real + l_disc_b_fake
 
-disc_params = []
-gen_params = []
+	#Total discriminator loss
+	l_disc = l_disc_a + l_disc_b
 
-for v in tf.trainable_variables():
-	if 'disc' in v.name:
-		disc_params.append(v)
-	if 'gen' in v.name:
-		gen_params.append(v)
-for q in disc_params:
-	print q
+	#Combined loss for individual generators
+	l_ga = l_gan_a + l_const_a
+	l_gb = l_gan_b + l_const_b
 
-for q in gen_params:
-	print q
+	#Total GAN loss
+	l_g = l_ga + l_gb
 
-trainer_D = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2)
-trainer_G = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2)
+	disc_params = []
+	gen_params = []
+	
+	gab_params = []
+	gba_params = []
+
+	disc_a_params = []
+	disc_b_params = []
+
+	for v in tf.trainable_variables():
+		if 'disc' in v.name:
+			disc_params.append(v)
+			if 'disc_a' in v.name:
+				disc_a_params.append(v)
+			if 'disc_b' in v.name:
+				disc_b_params.append(v)
+		if 'gen' in v.name:
+			gen_params.append(v)
+			if "gen_AB" in v.name:
+				gab_params.append(v)
+			if "gen_BA" in v.name:
+				gba_params.append(v)
+
+
+	trainer_Da = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2).minimize(l_disc_a,var_list=disc_a_params)
+	trainer_Db = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2).minimize(l_disc_b,var_list=disc_b_params)
+	#trainer_G = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2).minimize(l_g,var_list=gen_params)
+	trainer_Gab = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2).minimize(l_ga,var_list=gba_params)
+	trainer_Gba = tf.train.AdamOptimizer(LR,beta1=B1,beta2=B2).minimize(l_gb,var_list=gab_params)
+
+	init = tf.global_variables_initializer()
+
+	model_directory = "models"
+	
+	def post(im):
+		#return im
+		return (im+1.)/2
+
+	if not os.path.exists('a'):
+			os.makedirs('a')
+	if not os.path.exists('b'):
+			os.makedirs('b')
+	if not os.path.exists('ab'):
+			os.makedirs('ab')
+	if not os.path.exists('ba'):
+			os.makedirs('ba')
+
+	with tf.Session() as sess:
+		
+		sess.run(init)
+		saver = tf.train.Saver()
+
+		try:
+			saver.restore(sess, tf.train.latest_checkpoint("models"))
+		except:
+			print "Previous weights not found"		
+
+		for i in range(50000):
+	
+			realA = data.get_batch_a(BATCH_SIZE)
+			realB = data.get_batch_b(BATCH_SIZE)
+
+			_,_,daLoss,dbLoss = sess.run([trainer_Da,trainer_Db,l_disc_a,l_disc_b],feed_dict={x_a:realA,x_b:realB})	
+		
+			_,gaLoss = sess.run([trainer_Gab,l_ga],feed_dict={x_a:realA,x_b:realB})
+			_,gbLoss = sess.run([trainer_Gba,l_gb],feed_dict={x_a:realA,x_b:realB})
+
+			realA = data.get_batch_a(BATCH_SIZE)
+			realB = data.get_batch_b(BATCH_SIZE)
+
+			_,gaLoss = sess.run([trainer_Gab,l_ga],feed_dict={x_a:realA,x_b:realB})
+			_,gbLoss = sess.run([trainer_Gba,l_gb],feed_dict={x_a:realA,x_b:realB})
+			
+
+			print "Gen a Loss: " + str(gaLoss) + "\tGen b Loss: " + str(gaLoss) + "\tDisc a Loss: " + str(daLoss)+ "\tDisc b Loss: " + str(dbLoss) 
+			if i %10 == 0:
+				out_a,out_b,out_ab,out_ba = sess.run([g_ba,g_ab,g_aba,g_bab],feed_dict={x_a:realA,x_b:realB})
+				scipy.misc.imsave("a/gen"+str(i%500)+'.png',post(out_a[0]))
+				scipy.misc.imsave("b/gen"+str(i%500)+'.png',post(out_b[0]))
+				scipy.misc.imsave("ba/gen"+str(i%500)+'.png',post(out_ba[0]))
+				scipy.misc.imsave("ab/gen"+str(i%500)+'.png',post(out_ab[0]))
+			if i % 1000 == 0:
+				if not os.path.exists(model_directory):
+					os.makedirs(model_directory)
+				saver.save(sess,model_directory+'/model-'+str(i)+'.ckpt')
+				print "Saved Model"
+
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,21 @@
 import tensorflow as tf	
+
+class batch_norm(object):
+  def __init__(self, epsilon=1e-3, momentum = 0.9, name="batch_norm"):
+    with tf.variable_scope(name):
+      self.epsilon  = epsilon
+      self.momentum = momentum
+      self.name = name
+
+  def __call__(self, x, train=True):
+    return tf.contrib.layers.batch_norm(x,
+                      decay=self.momentum, 
+                      updates_collections=None,
+                      epsilon=self.epsilon,
+                      scale=True,
+                      is_training=train,
+                      scope=self.name)
+
 """
 Helper for convolution function given an input and conv weights
 """
@@ -15,14 +32,14 @@ def conv2d_t(x,w,shape,stride=2,padding="SAME"):
 """
 Helper for weight variable given shape. Does fancy initialization
 """
-def weight_var(shape,name='w'):
-	return tf.get_variable(name,shape)
+def weight_var(shape,name='w',init=tf.truncated_normal_initializer(stddev=0.02)):
+	return tf.get_variable(name,shape,initializer=init)
 
 """
 Same as above, but for bias
 """
 def bias_var(shape,name='b'):
-	return tf.get_variable(name,shape)
+	return tf.get_variable(name,shape,initializer=tf.constant_initializer(0.0))
 
 """
 Special activation function we'll be using: leaky relu
@@ -33,15 +50,15 @@ def lrelu(x,alpha=0.2):
 """
 Nice helper function for creating convolutional layers
 """
-def conv_layer(x,w_shape,b_shape,activation=tf.nn.relu,batch_norm=True,stride=2,name="conv2d",reuse=False):
+def conv_layer(x,w_shape,b_shape,activation=tf.nn.relu,batch_norm=None,stride=2,name="conv2d",reuse=False):
 	with tf.variable_scope(name) as scope:
 		if reuse:
 			scope.reuse_variables()
 		w = weight_var(w_shape,name="w"+name)
 		b = bias_var(b_shape,name="b"+name)
 		h = conv2d(x,w,stride=stride)+b
-		if batch_norm:
-			h = batch_norm_layer(h)
+		if batch_norm is not None:
+			h = batch_norm(h)
 		if activation:
 			h = activation(h)
 		return h
@@ -49,15 +66,15 @@ def conv_layer(x,w_shape,b_shape,activation=tf.nn.relu,batch_norm=True,stride=2,
 """
 Nice helper function for creating convolutional transpose layers
 """
-def conv_layer_t(x,w_shape,b_shape,shape,activation=tf.nn.relu,batch_norm=True,stride=2,name="deconv2d",reuse=False):
+def conv_layer_t(x,w_shape,b_shape,shape,activation=tf.nn.relu,batch_norm=None,stride=2,name="deconv2d",reuse=False):
 	with tf.variable_scope(name) as scope:
 		if reuse:
 			scope.reuse_variables()
 		w = weight_var(w_shape,name="w"+name)
 		b = bias_var(b_shape,name="b"+name)
 		h = conv2d_t(x,w,shape)+b
-		if batch_norm:
-			h = batch_norm_layer(h)
+		if batch_norm is not None:
+			h = batch_norm(h)
 		if activation:
 			h = activation(h)
 		return h
@@ -80,27 +97,45 @@ def fc_layer(x,w_shape,b_shape,activation=tf.nn.relu,batch_norm=True,dropout=Tru
 		h = tf.matmul(x,w)+b
 		if activation:
 			h = activation(h)
-		if batch_norm:
-			h = batch_norm_layer(h)
+		if batch_norm is not None:
+			h = batch_norm(h)
 		if dropout:
 			h = tf.nn.dropout(h,keep_prob)
 		return h
 
 """
+Huber loss function
+"""
+def huber_loss(logits,labels,max_gradient=1.0):
+	err = tf.abs(labels-logits)
+	mg = tf.constant(max_gradient)
+	lin = mg*(err-0.5*mg)
+	quad = 0.5*err*err
+	return tf.where(err<mg,quad,lin)
+
+"""
 Helper function to build Convolutional autoEncoder, used as the generator network
 """
-def generator(x,name="generator",reuse=False):
+def generator(x,batch_size,name="generator",reuse=False):
 	with tf.variable_scope(name) as scope:
 		if reuse:
 			scope.reuse_variables()
-		conv_1 = conv_layer(x,[4,4,int(x.get_shape()[-1]),64],[64],activation=lrelu,batch_norm=False,name="g_conv_1",reuse=reuse)
-		conv_2 = conv_layer(conv_1,[4,4,int(conv_1.get_shape()[-1]),64*2],[64*2],activation=lrelu,name="g_conv_2",reuse=reuse)
-		conv_3 = conv_layer(conv_2,[4,4,int(conv_2.get_shape()[-1]),64*4],[64*4],activation=lrelu,name="g_conv_3",reuse=reuse)
-		conv_4 = conv_layer(conv_3,[4,4,int(conv_3.get_shape()[-1]),64*8],[64*8],activation=lrelu,name="g_conv_4",reuse=reuse)
-		conv_t_1 = conv_layer_t(conv_4,[4,4,64,int(conv_4.get_shape()[-1])],[64],[-1,8,8,64],activation=lrelu,name="g_deconv_1",reuse=reuse)
-		conv_t_2 = conv_layer_t(conv_t_1,[4,4,32,int(conv_t_1.get_shape()[-1])],[32],[-1,16,16,32],activation=lrelu,name="g_deconv_2",reuse=reuse)
-		conv_t_3 = conv_layer_t(conv_t_2,[4,4,16,int(conv_t_2.get_shape()[-1])],[16],[-1,32,32,16],activation=lrelu,name="g_deconv_3",reuse=reuse)
-		conv_t_4 = conv_layer_t(conv_t_3,[4,4,3,int(conv_t_3.get_shape()[-1])],[3],[-1,64,64,3],batch_norm=False,name="g_deconv_4",reuse=reuse)
+
+		g_bn0 = batch_norm(name='g_bn0')
+		g_bn1 = batch_norm(name='g_bn1')
+		g_bn2 = batch_norm(name='g_bn2')
+		g_bn3 = batch_norm(name='g_bn3')
+		g_bn4 = batch_norm(name='g_bn4')
+		g_bn5 = batch_norm(name='g_bn5')
+
+		conv_1 = conv_layer(x,[4,4,int(x.get_shape()[-1]),64],[64],activation=lrelu,batch_norm=None,name="g_conv_1",reuse=reuse)
+		conv_2 = conv_layer(conv_1,[4,4,int(conv_1.get_shape()[-1]),64*2],[64*2],activation=lrelu,batch_norm=g_bn0,name="g_conv_2",reuse=reuse)
+		conv_3 = conv_layer(conv_2,[4,4,int(conv_2.get_shape()[-1]),64*4],[64*4],activation=lrelu,batch_norm=g_bn1,name="g_conv_3",reuse=reuse)
+		conv_4 = conv_layer(conv_3,[4,4,int(conv_3.get_shape()[-1]),64*8],[64*8],activation=lrelu,batch_norm=g_bn2,name="g_conv_4",reuse=reuse)
+		conv_t_1 = conv_layer_t(conv_4,[4,4,64,int(conv_4.get_shape()[-1])],[64],[batch_size,8,8,64],activation=lrelu,batch_norm=g_bn3,name="g_deconv_1",reuse=reuse)
+		conv_t_2 = conv_layer_t(conv_t_1,[4,4,32,int(conv_t_1.get_shape()[-1])],[32],[batch_size,16,16,32],activation=lrelu,batch_norm=g_bn4,name="g_deconv_2",reuse=reuse)
+		conv_t_3 = conv_layer_t(conv_t_2,[4,4,16,int(conv_t_2.get_shape()[-1])],[16],[batch_size,32,32,16],activation=lrelu,batch_norm=g_bn5,name="g_deconv_3",reuse=reuse)
+		conv_t_4 = conv_layer_t(conv_t_3,[4,4,3,int(conv_t_3.get_shape()[-1])],[3],[batch_size,64,64,3],activation=None,batch_norm=None,name="g_deconv_4",reuse=reuse)
 
 		out = conv_t_4
 	
@@ -113,11 +148,17 @@ def discriminator(x,name="discriminator",reuse=False):
 	with tf.variable_scope(name) as scope:
 		if reuse:
 			scope.reuse_variables()
-		conv_1 = conv_layer(x,[4,4,int(x.get_shape()[-1]),32],[32],activation=lrelu,batch_norm=False,name="d_conv_1",reuse=reuse)
-		conv_2 = conv_layer(conv_1,[4,4,int(conv_1.get_shape()[-1]),16],[16],activation=lrelu,name="d_conv_2",reuse=reuse)
-		conv_3 = conv_layer(conv_2,[4,4,int(conv_2.get_shape()[-1]),8],[8],activation=lrelu,name="d_conv_3",reuse=reuse)
-		conv_4 = conv_layer(conv_3,[4,4,int(conv_3.get_shape()[-1]),4],[4],activation=lrelu,name="d_conv_4",reuse=reuse)
-		out = conv_layer(conv_4,[4,4,int(conv_4.get_shape()[-1]),1],[1],activation=None,stride=4,batch_norm=False,name="d_conv_5",reuse=reuse)
+
+		d_bn0 = batch_norm(name='d_bn0')
+		d_bn1 = batch_norm(name='d_bn1')
+		d_bn2 = batch_norm(name='d_bn2')		
+
+		conv_1 = conv_layer(x,[4,4,int(x.get_shape()[-1]),32],[32],activation=lrelu,batch_norm=None,name="d_conv_1",reuse=reuse)
+		conv_2 = conv_layer(conv_1,[4,4,int(conv_1.get_shape()[-1]),16],[16],activation=lrelu,batch_norm=d_bn0,name="d_conv_2",reuse=reuse)
+		conv_3 = conv_layer(conv_2,[4,4,int(conv_2.get_shape()[-1]),8],[8],activation=lrelu,batch_norm=d_bn1,name="d_conv_3",reuse=reuse)
+		conv_4 = conv_layer(conv_3,[4,4,int(conv_3.get_shape()[-1]),4],[4],activation=lrelu,batch_norm=d_bn2,name="d_conv_4",reuse=reuse)
+		out = conv_layer(conv_4,[4,4,int(conv_4.get_shape()[-1]),1],[1],activation=None,stride=4,batch_norm=None,name="d_conv_5",reuse=reuse)
 		out = tf.squeeze(out)
-		return out	
+		return out
+
 	
